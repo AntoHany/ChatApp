0d6dd5306189538105f1c9e3e6d13118e305f590
antohany <antonhany67@gmail.com>
2024-08-28 15:16:05 +0300

    manage socketIo in clint

--- clint/src/socket.js
+++ clint/src/socket.js
@@ -2,5 +2,7 @@ import { io } from 'socket.io-client';
 import { useURL } from './store/store';
  
 export const socket = io.connect(useURL,{
-  transports: ['websocket'],
+  transports: ['websocket'], // Use WebSocket transport only
+  reconnection: true,        // Enable reconnection attempts
+  timeout: 20000,            // Set timeout to 20 seconds
 });
